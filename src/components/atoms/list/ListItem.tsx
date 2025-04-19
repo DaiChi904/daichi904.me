e1b8e939721bc36890b7a2a21e0ccebf4487cf8c
Daichi Uematsu <minimi.904.horo@gmail.com>
2025-04-19 18:54:32 +0900

    run biome

--- src/components/atoms/list/ListItem.tsx
+++ src/components/atoms/list/ListItem.tsx
@@ -3,5 +3,9 @@ import type { HTMLAttributes } from "react";
 interface ListItemProps extends HTMLAttributes<HTMLLIElement> {}
 
 export default function ListItem({ children, ...props }: ListItemProps) {
-  return <li {...props} className={`text-black dark:text-white ${props.className}`}>{children}</li>;
+  return (
+    <li {...props} className={`text-black dark:text-white ${props.className}`}>
+      {children}
+    </li>
+  );
 }
